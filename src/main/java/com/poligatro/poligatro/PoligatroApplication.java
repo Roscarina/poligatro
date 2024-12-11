43c2d39673588a22a384d824f814dfc719ad6b8a
Oscarina Romero <roscarina@gmail.com>
2024-12-11 00:18:16 -0500

    inicio

---
+++ src/main/java/com/poligatro/poligatro/PoligatroApplication.java
@@ -0,0 +1,13 @@
+package com.poligatro.poligatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoligatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoligatroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poligatro/poligatro/PoligatroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poligatro.poligatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoligatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
